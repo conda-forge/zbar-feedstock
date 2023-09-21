401bba39966bd95a5525631027c9e31e32785e2c
Jan Janssen <jan-janssen@users.noreply.github.com>
2023-09-21 16:46:24 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-./configure --prefix=$PREFIX  --disable-video --without-gtk --without-python --without-qt --without-imagemagick --without-xv --without-x --without-xshm
+./configure --prefix=$PREFIX  --disable-silent-rules --disable-video --without-gtk --without-python --without-qt --without-imagemagick --without-xv --without-x --without-xshm
 make
 make check
 make install
