a58b07a2d03bb3133c0e1f4dec8986f5e8f7280a
Jan Janssen <jan-janssen@users.noreply.github.com>
2023-09-21 17:45:24 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-automake
+aclocal
+autoreconf -i
+automake --add-missing
+autoconf
 ./configure --prefix=$PREFIX  --disable-video --without-gtk --without-python --without-qt --without-imagemagick --without-xv --without-x --without-xshm
 make
 make check
