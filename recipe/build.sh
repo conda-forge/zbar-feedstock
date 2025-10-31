94e1c58e8c8e700ae8e45373bd84b39f4104ac3e
Carlo <carlodri@users.noreply.github.com>
2025-10-31 10:03:41 +0100

    Fix autoreconf command syntax in build script

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,7 +4,7 @@ set -exo pipefail
 # Get an updated config.sub and config.guess
 # cp $BUILD_PREFIX/share/gnuconfig/config.* ./config
 
-./autoreconf -vfi
+autoreconf -vfi
 
 ./configure --prefix=$PREFIX  --disable-video --without-gtk --without-python --without-qt --without-imagemagick --without-xv --without-x --without-xshm
 make
