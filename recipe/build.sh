cd967d6925204f0cdcb3dd702a1ff6152183cef8
Carlo <carlodri@users.noreply.github.com>
2025-10-31 09:52:19 +0100

    Refactor build.sh
    Following https://github.com/mchehab/zbar/blob/master/INSTALL.md

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,10 +1,14 @@
-#!/bin/bash
+#! /bin/bash
+
+set -exo pipefail
 # Get an updated config.sub and config.guess
-cp $BUILD_PREFIX/share/gnuconfig/config.* ./config
+# cp $BUILD_PREFIX/share/gnuconfig/config.* ./config
+
+./autoreconf -vfi
 
 ./configure --prefix=$PREFIX  --disable-video --without-gtk --without-python --without-qt --without-imagemagick --without-xv --without-x --without-xshm
 make
-if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "" ]]; then
+# if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "" ]]; then
 make check
-fi
+# fi
 make install
