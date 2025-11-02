add44f2e38ad155ca1c65219d436f22b834337f6
Carlo Dri <carlo.dri@gmail.com>
2025-11-02 23:26:09 +0100

    try a fix for arm64

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,12 +3,12 @@
 set -exo pipefail
 # Get an updated config.sub and config.guess
 # cp $BUILD_PREFIX/share/gnuconfig/config.* ./config
+export PYTHON=$PREFIX/bin/python
 
 autoreconf -vfi
 
 ./configure --prefix=$PREFIX  --disable-video --without-gtk --without-python --without-qt --without-imagemagick --without-xv --without-x --without-xshm
+
 make
-# if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "" ]]; then
-# make check
-# fi
+
 make install
