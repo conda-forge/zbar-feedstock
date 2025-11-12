735cc0956470718dccb3cd55a2df43438faf2dc2
Carlo <carlodri@users.noreply.github.com>
2025-11-12 21:35:03 +0100

    Clean up build.sh by removing comments and export
    Removed unnecessary comments and export statement.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,9 +1,6 @@
 #! /bin/bash
 
 set -exo pipefail
-# Get an updated config.sub and config.guess
-# cp $BUILD_PREFIX/share/gnuconfig/config.* ./config
-export PYTHON=$PREFIX/bin/python
 
 autoreconf -vfi
 
