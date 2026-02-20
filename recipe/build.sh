6911819184cb063cc9c2deaa901e59f5f034d05f
Carlo <carlo.dri@gmail.com>
2026-02-20 09:02:31 +0100

    try a suggestion from claude sonnet 4.6

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,16 +1,33 @@
 #!/bin/bash
-
 set -exo pipefail
 
 autoreconf -vfi
-./configure --prefix=$PREFIX \
-            --disable-video \
-            --without-gtk \
-            --without-python \
-            --without-qt \
-            --without-imagemagick \
-            --without-xv \
-            --without-x \
-            --without-xshm
+
+# autotools_clang_conda provides this on Windows to fix libtool DLL generation;
+# on Unix it is not present so we skip it safely.
+if [[ "${target_platform}" == win-* ]]; then
+    patch_libtool
+fi
+
+CONFIGURE_ARGS=(
+    --prefix=$PREFIX
+    --disable-video
+    --without-gtk
+    --without-python
+    --without-qt
+    --without-imagemagick
+    --without-xv
+    --without-x
+    --without-xshm
+)
+
+if [[ "${target_platform}" == win-* ]]; then
+    CONFIGURE_ARGS+=(--with-directshow)
+else
+    CONFIGURE_ARGS+=(--without-directshow)
+fi
+
+./configure "${CONFIGURE_ARGS[@]}"
+
 make
 make install
